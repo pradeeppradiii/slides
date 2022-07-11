54a7eddc96a2c04cbba64e3289f96f6dac8fb820
pradeeppradiii <pradeepfoxy198@gmail.com>
2022-07-11 19:32:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'h8k-components';
+import Slides from './components/Slides';
+const title = "Slideshow App";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h8k-navbar header={title}></h8k-navbar>
+            <div className="App">
+                <Slides slides={slides} />
+            </div>
     </div>
   );
 }
